fb241195b175a6a204b813ed5cd8b18bd1cd116e
Daniel <dccote@cervo.ulaval.ca>
2022-03-24 14:52:54 -0400

    Starting a test file for unittesting database

---
+++ testBuildingDatabase.py
@@ -0,0 +1,11 @@
+import unittest
+import numpy as np
+from dcclab import Database
+
+class TestBuildDatabase(unittest.TestCase):
+    def testDatabase(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
