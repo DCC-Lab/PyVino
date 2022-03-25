7f85b69a826a4592df189adf3423d542b34c598f
Daniel <dccote@cervo.ulaval.ca>
2022-03-25 17:19:45 -0400

    Labels and smaller database.
    Added labels to getIntensities()
    Removed md5 from spectra, now linking to a file identifier key (much smaller database)

--- testDatabase.py
+++ testDatabase.py
@@ -33,21 +33,38 @@ class TestBuildDatabase(unittest.TestCase):
 
     def testGetIntensity(self):
         db = RamanDB()
-        matrix = db.getIntensities()
+        matrix, labels = db.getIntensities()
         self.assertIsNotNone(matrix)
         self.assertEqual(matrix.shape, (len(db.wavelengths), db.getCountFiles()))
-        
+
+    @unittest.skip("Ok, tested")
     def testDownload(self):
         url = 'https://www.dropbox.com/s/2st0sv7jpii6dz8/raman.db?dl=1'
         r = requests.get(url, allow_redirects=True)
         with open('test.db', 'wb') as file:
             file.write(r.content)
 
+    @unittest.skip("Ok, tested")
     def testDownload(self):
         db = RamanDB()
         filename = db.downloadDatabase()
         self.assertTrue(os.path.exists(filename))
         os.remove(filename)
+
+    @unittest.skip("Done, no need to redo.")
+    def testAddFileIdToDatabase(self):
+        db = RamanDB(writePermission=True)
+        db.execute("select * from files order by path")
+        records = db.fetchAll()
+        for i, record in enumerate(records):
+            db.execute("update files set fid={0} where md5='{1}'".format(i, record["md5"]))
+
+        db.execute("select spectra.md5, files.fid from spectra inner join files on files.md5 = spectra.md5")
+        records = db.fetchAll()
+        for i, record in enumerate(records):
+            statement = "update spectra set fid={0} where md5='{1}'".format(record["fid"], record["md5"])
+            db.execute(statement)
+
 
 if __name__ == "__main__":
     unittest.main()
